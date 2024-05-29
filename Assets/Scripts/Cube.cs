4eceeac442f1e07548008649e4fd339855da22fc
levakulitkov <levakulitkov@yandex.ru>
2024-05-29 07:27:26 +0500

    доб. взрыв, если не произошло разделение

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -8,6 +8,8 @@ public class Cube : MonoBehaviour
     [SerializeField] private float _startSeparationChance = 1f;
     [SerializeField] private float _startExplosuionRadius = 5f;
     [SerializeField] private float _startExplosuionForce = 100f;
+    [SerializeField] private float _startGlobalExplosuionRadius = 5f;
+    [SerializeField] private float _startGlobalExplosuionForce = 100f;
 
     private Renderer _renderer;
     private BoxCollider _collider;
@@ -15,6 +17,8 @@ public class Cube : MonoBehaviour
     private float _separationChance;
     private float _explosuionRadius;
     private float _explosuionForce;
+    private float _globalExplosuionRadius;
+    private float _globalExplosuionForce;
 
     private void Awake()
     {
@@ -25,6 +29,8 @@ public class Cube : MonoBehaviour
         _separationChance = _startSeparationChance;
         _explosuionRadius = _startExplosuionRadius;
         _explosuionForce = _startExplosuionForce;
+        _globalExplosuionRadius = _startGlobalExplosuionRadius;
+        _globalExplosuionForce = _startGlobalExplosuionForce;
     }
 
     private void OnMouseUpAsButton()
@@ -37,13 +43,21 @@ public class Cube : MonoBehaviour
             foreach (Rigidbody explodableCube in explodableCubes)
                 explodableCube.AddExplosionForce(_explosuionForce, transform.position,
                     _explosuionRadius);
-        }        
+        }
+        else
+        {
+            List<Rigidbody> explodableObjects = GetExplodableObjects();
+            foreach (Rigidbody explodableObject in explodableObjects)
+                explodableObject.AddExplosionForce(_globalExplosuionForce, 
+                    transform.position, _globalExplosuionRadius);
+        }
 
         Destroy(gameObject);
     }
 
     public void Init(Vector3 prevScale, float prevMass, float prevSeparationChance,
-        float prevExplosuionForce, float prevExplosuionRadius)
+        float prevExplosuionForce, float prevExplosuionRadius,
+        float prevGlobalExplosuionForce, float prevGlobalExplosuionRadius)
     {
         transform.localScale = prevScale / 2;
 
@@ -54,6 +68,8 @@ public class Cube : MonoBehaviour
         _separationChance = prevSeparationChance / 2;
         _explosuionForce = prevExplosuionForce / 2;
         _explosuionRadius = prevExplosuionRadius / 2;
+        _globalExplosuionForce = prevGlobalExplosuionForce * 2;
+        _globalExplosuionRadius = prevGlobalExplosuionRadius * 2;
     }
 
     private Cube[] SpawnCubes()
@@ -67,9 +83,23 @@ public class Cube : MonoBehaviour
             cubes[i] = Instantiate(this, transform.parent);
 
             cubes[i].Init(transform.localScale, _rb.mass, _separationChance, 
-                _explosuionForce, _explosuionRadius);
+                _explosuionForce, _explosuionRadius, _globalExplosuionForce, 
+                _globalExplosuionRadius);
         }
 
         return cubes;
+    }
+
+    private List<Rigidbody> GetExplodableObjects()
+    {
+        var explodableObjects = new List<Rigidbody>();
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _globalExplosuionRadius);
+
+        foreach (Collider collider in colliders)
+            if(collider.TryGetComponent(out Rigidbody rigidbody))
+                explodableObjects.Add(rigidbody);
+
+        return explodableObjects;
     }
 }
